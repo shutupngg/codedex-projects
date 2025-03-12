98a3053b3f7adc4866d8f2e3bea7c2af21dacf4c
shutupngg <161845903+shutupngg@users.noreply.github.com>
2025-03-12 14:22:22 +0100

    Update main.js

--- fizz-buzz/main.js
+++ fizz-buzz/main.js
@@ -1,1 +1,10 @@
-
+var n = 15;
+for (i=1; i<=n;i++){
+  if(i%3==0 && i%5==0){
+    console.log("Fizz Buzz")
+  } else if (i%3==0){
+    console.log("Fizz")
+  } else if (i%5==0){
+    console.log("Buzz")
+  } else {console.log(i)}
+}
